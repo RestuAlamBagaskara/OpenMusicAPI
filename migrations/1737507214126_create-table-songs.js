09e76725fdb1143b714f96e84c86980e6f0b6570
alam <restu.bagaskara@harrison-gil.com>
2025-01-22 11:50:56 +0700

    Submission 1

---
+++ migrations/1737507214126_create-table-songs.js
@@ -0,0 +1,56 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    year: {
+      type: 'INT',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    performer: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    duration: {
+      type: 'INT',
+      notNull: false,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      references: 'albums(id)',
+      notNull: false,
+      onDelete: 'SET NULL',
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
---
+++ src/server.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const albums = require('./api/albums');
+const SongsService = require('./services/postgres/SongService');
+const AlbumsService = require('./services/postgres/AlbumService');
+const SongsValidator = require('./validator/songs');
+const AlbumsValidator = require('./validator/albums');
+const ClientError = require('./exceptions/ClientError');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 5000,
+    host: process.env.HOST || 'localhost',
+    routes: {
+      cors: {
+        origin: ['*']
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: new SongsService(),
+        validator: SongsValidator,
+      },
+    },
+    {
+      plugin: albums,
+      options: {
+        service: new AlbumsService(),
+        validator: AlbumsValidator,
+      },
+    },
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
